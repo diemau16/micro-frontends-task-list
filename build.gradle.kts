ab3e0fa8b08d0703f773e000284f9a5ff61e099b
Diego Acosta <diemauacro@gmail.com>
2023-09-15 03:09:18 -0500

    Add spring and mysql dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java:8.0.30")
 }
 
 tasks.withType<Test> {
